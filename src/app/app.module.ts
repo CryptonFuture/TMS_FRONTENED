d6246c52a664d9e6cdb562aab3521fe1cb115577
abrar <moosaabrar01@gmail.com>
2025-07-16 20:21:35 +0500

    loginscreen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginscreenModule } from './feature/auth/loginscreen/loginscreen.module';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+  BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    LoginscreenModule,
+    AppRoutingModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/feature/auth/loginscreen/loginscreen.module.ts
@@ -0,0 +1,39 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { LoginscreenComponent } from './loginscreen.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+@NgModule({
+    imports: [
+          
+            FormsModule,
+            ReactiveFormsModule,
+            BrowserAnimationsModule,
+            MatCardModule,
+            MatFormFieldModule,
+            MatInputModule,
+            MatCheckboxModule,
+            MatButtonModule
+        
+           
+
+    ],
+    declarations: [
+        LoginscreenComponent,
+    ],
+    exports: [
+        LoginscreenComponent,
+    ]
+})
+export class LoginscreenModule {
+
+}
